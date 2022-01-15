b84ccee0cbb13ad91b886db495eced52e2c986ab
somudevops <96955041+somudevops@users.noreply.github.com>
2022-01-15 08:34:35 +0530

    Update backend.tf

--- terraform own code/backend.tf
+++ terraform own code/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "azurerm" {
-    subscription_id      = "4012f1b9-10e6-4fd3-bd77-a0d45b5a62f9"
-    tenant_id            = "ca3e5489-e140-4de4-83a3-964e352de686"
+    subscription_id      = "12c0d075-059f-4523-8432-5eee63b9ac55"
+    tenant_id            = "26f384ce-1b16-4dae-80ea-4339d5399073"
+    client_id            = "a72c02ac-337a-448c-86eb-a37b237d8e8d"
     resource_group_name  = "Mani_RG"
     storage_account_name = "manistrg0801"
     container_name       = "manicontainer"
